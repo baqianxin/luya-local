3772f7b4443390a3fd4bb4bb1fd45d32b84d5154
oom-home <oom@home.com>
2017-12-28 23:46:20 +0800

    page layout

--- views/cmslayouts/simple.php
+++ views/cmslayouts/simple.php
@@ -1,7 +1,7 @@
 <div class="container" id="content">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <?= $placeholders['left']; ?>
+            <?= $placeholders['content']; ?>
         </div>
     </div>
 </div>
